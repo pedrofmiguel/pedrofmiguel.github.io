96e4fce1954a83aa87c74b3441bda488f6dd855d
Pedro Ferreira <pedrofmiguel@outlook.pt>
2020-07-05 01:50:00 +0100

    feat - Scss, Barba, Timeline => alteração completa do website, para um design mais clean.
    todo:responsivo.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,71 @@
+'use strict'
 
-window.onload = function(){ 
-console.log('Main js Loaded');
+let loading = document.querySelector('.loading')
+let nav;
 
-particlesJS.load('particles-js','assets/particles.json', function(){
-  console.log('callback - particles.js config loaded');
-});
+const delay = (n) => {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+
+}
+
+const pageTransition = () => {
+  let tl = gsap.timeline();
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+  });
+
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+const contentAnimation = () => {
+
+  setTimeout(() => {
+    document.querySelector('.left-name').classList.add('left-name-show')
+  }, 200)
+  setTimeout(() => {
+    document.querySelector('.left-title').classList.add('left-title-show')
+  }, 1000)
+
+  let tl = gsap.timeline();
+  tl.from('.left', { duration: 1.5, translateY: 50, opacity: 0 })
+  tl.to('img', { clipPath: "polygon(0 0 , 100% 0 ,100% 100%,  0% 100%)" }, "-=1.1")
+}
+
+window.onload = () => {
+  barba.init({
+    transitions: [{
+      async leave(data) {
+        const done = this.async();
+        pageTransitionIn()
+        await delay(1500)
+        data.current.container.remove()
+        done()
+      },
+
+      async enter(data) {
+        contentAnimation();
+      },
+
+      async once(data) {
+        contentAnimation();
+      }
+    }]
+  });
 }
 
 
