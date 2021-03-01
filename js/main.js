92b578c4520e276abcaba3cdbd1047835307fd7a
Pedro Ferreira <pedrofmiguel@outlook.pt>
2021-03-01 22:40:22 +0000

    New version of website
    - Changed color scheme,
    - changed cursor
    ´
    TODO: implement barba, responsive.

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ const pageTransition = () => {
 }
 
 const contentAnimation = () => {
-
   setTimeout(() => {
     document.querySelector('.left-name').classList.add('left-name-show')
   }, 200)
@@ -43,7 +42,9 @@ const contentAnimation = () => {
 
   let tl = gsap.timeline();
   tl.from('.left', { duration: 1.5, translateY: 50, opacity: 0 })
-  tl.to('img', { clipPath: "polygon(0 0 , 100% 0 ,100% 100%,  0% 100%)" }, "-=1.1")
+  tl.to('.right img', { clipPath: "polygon(0 0 , 100% 0 ,100% 100%,  0% 100%)" }, "-=1.1")
+  tl.to('.hoverables', { duration: 1.5, translateY: 50, opacity: 1 })
+
 }
 
 window.onload = () => {
@@ -63,6 +64,40 @@ window.onload = () => {
 
       async once(data) {
         contentAnimation();
+        const $bigBall = document.querySelector('.cursor__ball--big');
+        const $smallBall = document.querySelector('.cursor__ball--small');
+        const $hoverables = document.querySelectorAll('.hoverable');
+    
+        // Listeners
+        document.body.addEventListener('mousemove', onMouseMove);
+        for (let i = 0; i < $hoverables.length; i++) {
+            $hoverables[i].addEventListener('mouseenter', onMouseHover);
+            $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+        }
+    
+        // Move the cursor
+        function onMouseMove(e) {
+            TweenMax.to($bigBall, .4, {
+                x: e.pageX - 15,
+                y: e.pageY - 15
+            })
+            TweenMax.to($smallBall, .1, {
+                x: e.pageX - 5,
+                y: e.pageY - 7
+            })
+        }
+    
+        // Hover an element
+        function onMouseHover() {
+            TweenMax.to($bigBall, .3, {
+                scale: 4
+            })
+        }
+        function onMouseHoverOut() {
+            TweenMax.to($bigBall, .3, {
+                scale: 1
+            })
+        }
       }
     }]
   });
